9097255168830e0ad9ee72353ab7fbb4bc326ac7
fiuzatayna <taynafiuza2@gmail.com>
2022-05-17 16:56:08 -0300

    Adiciona funções de preparação dos dados

---
+++ src/data/prepare.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import pandas as pd
+
+
+def read_texts(path):
+    data_path = Path(path)
+    texts = []
+    for file_path in data_path.glob("*.txt"):
+        with open(file_path, 'rt') as file:
+            texts.append(file.read())
+    return texts
+
+def make_text_dataframe(true_path, fake_path, output_path):
+    true_texts = read_texts(true_path)
+    fake_texts = read_texts(fake_path)
+    text_df = pd.DataFrame({
+        'text': true_texts + fake_texts,
+        'label': len(true_texts) * ['true'] + len(fake_texts) * ['fake']
+    })
+    text_df.to_csv(output_path)
