3792d367041026685996cdb113d0d9df58743df0
fiuzatayna <taynafiuza2@gmail.com>
2022-05-24 17:44:08 -0300

    Adiciona função show_ngrams

--- src/data/visualize.py
+++ src/data/visualize.py
@@ -16,3 +16,17 @@ def show_wordcloud(data, stopwords, title = None):
         fig.subplots_adjust(top=1)
 
     plt.imshow(wordcloud)
+    
+def show_ngrams(true_n_gram, fake_n_gram):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 8))
+    fig.suptitle('N-gramas mais presentes nas notícias')
+
+    x_true,y_true=map(list,zip(*true_n_gram))
+    ax1.barh(y=x_true[::-1], width=y_true[::-1], color='green')
+    ax1.title.set_text("Verdadeiras")
+
+    x_fake,y_fake=map(list,zip(*fake_n_gram))
+    ax2.barh(y=x_fake[::-1], width=y_fake[::-1], color='red')
+    ax2.title.set_text("Falsas")
+    ax2.invert_xaxis()
+    ax2.yaxis.tick_right()
