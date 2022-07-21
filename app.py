cd5520f7db6aebc1eb23b6ece40bb8e41c5d6448
Tayna Fiuza <taynafiuza2@gmail.com>
2022-07-21 11:23:50 -0300

    Tira layout wide

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # bibliotecas
-import streamlit as st # site
-from st_on_hover_tabs import on_hover_tabs # menus laterais
-from src.app import verificador, analise_exploratoria, analise_comparativa, sobre
+import streamlit as st  # site
+from st_on_hover_tabs import on_hover_tabs  # menus laterais
+
+from src.app import (analise_comparativa, analise_exploratoria, sobre,
+                     verificador)
 
 # configuração
-st.set_page_config(layout='wide') # página em modo amplo
+#st.set_page_config(layout='wide') # página em modo amplo
 
 # st.header("Custom tab component for on-hover navigation bar")
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
